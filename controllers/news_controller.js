f0f929930573da028feabc811b7e3701b6fe312a
Mahmudul-Sumoon <sumoon14@cse.pstu.ac.bd>
2021-12-31 00:05:58 +0600

    initial commit

---
+++ controllers/news_controller.js
@@ -0,0 +1,26 @@
+
+const { default: axios } = require("axios");
+var uuid = require('uuid');
+
+const getNews = async (req,res)=>{
+    try{
+        var responseObj = [];
+      var data = await axios.get(process.env.url);
+      data.data.articles.map((e) => {
+        responseObj.push({
+            "id": uuid.v4(),
+            "title":e.title,
+            "description":e.description,
+            "url":e.url,
+            "urlToImage":e.urlToImage,
+        });
+      });
+    }
+    catch(e){
+        console.log(e);
+    }
+    res.json(responseObj);
+    
+   };
+
+module.exports = {getNews};
